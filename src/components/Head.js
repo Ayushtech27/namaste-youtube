f0337866c53b6eee44426ee1ee977663b06a6c63
Ayush Singhal <singhal.ayush27@gmail.com>
2024-03-07 05:05:15 +0530

    Initial dynamic search suggestions with debouncing

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
 
 const Head = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
   const dispatch = useDispatch();
+  useEffect(() => {
+    //API Call
+    /**
+     * Make an API call after every keystrokes
+     * but if the difference between two key strokes is < 200ms
+     * decline the api call
+     */
+
+    // Debouncing
+    const timer = setTimeout(() => getSearchSuggestions(), 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    // console.log(json[1]);
+    setSuggestions(json[1]);
+  };
 
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
@@ -27,13 +52,26 @@ const Head = () => {
         </a>
       </div>
       <div className="col-span-10 px-10">
-        <input
-          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
-          type="text"
-        />
-        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
-          🔍
-        </button>
+        <div>
+          <input
+            className="w-1/2 border border-gray-400 p-2 rounded-l-full px-5"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+            🔍
+          </button>
+        </div>
+        <div className="fixed bg-white py-2 px-2 w-[30.5rem] shadow-lg rounded-lg border border-gray-100">
+          <ul>
+            {suggestions.map((s) => (
+              <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
+                🔍 {s}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div className="col-span-1">
         <img
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,9 @@ const GOOGLE_API_KEY = "AIzaSyA99N7SIlXgkLExqNQN93xoljth9SD8lCo";
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
+
+export const YOUTUBE_SEARCH_API =
+  "https://corsproxy.org/?" +
+  encodeURIComponent(
+    "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+  );
