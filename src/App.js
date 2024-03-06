9977e30ea05f8ad11a2a95bbdf9495b1a0f06fa8
Ayush Singhal <singhal.ayush27@gmail.com>
2024-03-07 02:24:30 +0530

    Creating initial WatchPage and routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Head from "./components/Head";
 import Body from "./components/Body";
 import { Provider } from "react-redux";
 import store from "./utils/store";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 /**
  * Head
@@ -15,12 +18,29 @@ import store from "./utils/store";
  *     -VideoCard
  */
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "/watch",
+        element: <WatchPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
   return (
     <Provider store={store}>
       <div>
         <Head />
-        <Body />
+        <RouterProvider router={appRouter} />
       </div>
     </Provider>
   );
